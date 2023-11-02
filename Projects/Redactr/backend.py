0213e86ab8034fc8ba35830325c0eb951b2e3d83
mfonekpo <mfonekpo34@gmail.com>
2023-11-02 23:41:23 +0100

    docker containerizing complete

--- Projects/Redactr/backend.py
+++ Projects/Redactr/backend.py
@@ -7,10 +7,12 @@ import uvicorn
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="../static"), name="static")
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 
-templates = Jinja2Templates(directory="../templates")
+# templates = Jinja2Templates(directory="../templates")
+templates = Jinja2Templates(directory="./templates")
 
 @app.get("/", response_class=HTMLResponse)
 def render_page(request: Request):
@@ -29,11 +31,7 @@ def redact_text(request: Request, content: str = Form(...), words: str = Form(..
                "duration": f"{round(duration, 2)}"
                }
     return templates.TemplateResponse("index.html", context)
-    
-    # [Redaction logic remains the same...]
 
 
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+    # uvicorn.run(app, host="0.0.0.0", port=8888)
